6402cfd52f785131bdf10ff9a69c1520935e3526
samundeeswari25 <samusmart25@gmail.com>
2026-06-15 16:43:47 +0530

    Framework

--- src/test/java/runnerPack/runnerClass.java
+++ src/test/java/runnerPack/runnerClass.java
@@ -1,5 +1,11 @@
 package runnerPack;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "",glue = "",plugin = {"pretty"})
 public class runnerClass {
 
 
